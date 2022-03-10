3b46647fbfda02b6d5026b0970df0d221fb1fa90
ale777opp <ale777opp@yandex.ru>
2022-03-10 22:20:41 +0300

    grid+vue, no css, no switching

--- main.js
+++ main.js
@@ -4,54 +4,80 @@ props: {
 	required: true
 },
 template:
-`<ul>
-  <li v-for="item in items">{{ item }}</li>
-</ul>`
+`
+<span {{ item }}</span>
+`
 })
 
 var menu = new Vue({
 	el: '#mainMenu',
 	data: {
-		selectedItem: 0,
+		activeItem: 0,
 		menuItems: [
 		{
-		item : 'О библиотеке',
+		id: 1,	
+		title : 'О библиотеке',
 		flag: true,
-		subMenu: ['История','Адрес и время работы','3D панорамы Галерея','Противодействие коррупции','Структура библиотеки, контакты','Реквизиты библиоттеки','Официальные документы','Попечительский совет']
+		subMenu: ['История','Адрес и время работы','3D панорамы Галерея','Противодействие коррупции','Структура библиотеки, контакты','Реквизиты библиотеки','Официальные документы','Попечительский совет']
 		},
 
 		{
-		item : 'Читателям',
+		id: 2,	
+		title : 'Читателям',
 		flag: false,
 		subMenu: ['Запись читателей','Услуги, правила пользования','Фонды, ресурсы, каталоги','Доступная среда','Мероприятия и экскурсии','Клубы и объединения','Учёба в РГБИ','Творческое развитие']
 		},
-		
+
 		{
-		item :  'Коллегам',
+		id: 3,	
+		title :  'Коллегам',
 		flag: false,
 		subMenu: ['Конференции, семинары','Методические документы','Проекты библиотеки','Издания РГБИ','Библиотека благодарит','Творческие конкурсы','Вакансии','Секция библиотек по искуству и музейных библиотек РБА']
 		},
 
 		{
-		item : 'Спроси библиографа',
+		id: 4,	
+		title : 'Спроси библиографа',
 		flag: false,
 		subMenu: ['Отзывы и предложения','Тематические запросы','Вне категорий','Наличие изданий','Вопросы о работе РГБИ','Задать вопрос']
-    	},
-    	]
+    },
+
+    ]
     },	
 
     methods: {
     selectSubMenu: function (index) {
-    	this.menuItems[index].flag = true;
-    //	this.selectedItem = index;
-    //this.menuItems[index].flag? this.menuItems[index].flag = false : this.menuItems[index].flag = true;
-    //console.log(index); //event.target.innerHTML
-    //console.log(this.menuItems[index].flag);
+    this.activeItem = this.activeItem + 0;
+    if (this.activeItem =! index) {
+    	console.log(typeof(this.activeItem));
+    	console.log(typeof(index));
+    	console.log(this.menuItems[this.activeItem]);
+    	console.log(this.menuItems[index]);
+    }
+    //	this.menuItems[index].flag = true;
+    //	this.activeItem = index;
+    //console.log(this.menuItems.flag.includes(false)); //event.target.innerHTML
+    //	console.log(this.activeItem =! index);
+    //.find(item => item.flag == true)
     },
     unselectSubMenu: function (index) {
-    //	this.selectedItem = index;
-    	this.menuItems[index].flag = false;
+      	this.menuItems[index].flag = false;
+      	this.activeItem = index;
+    	console.log(this.menuItems[index].title);
+    	console.log(this.activeItem);
+    	//console.log(this.menuItems.findIndex(item => item.flag == true))
+    	//if (this.menuItems.findIndex(item => item.flag == true) == -1) this.menuItems[index].flag = true;
     },
-  	}
-});
 
+  	mainMenuClass(index) {
+  		//console.log(index);
+     	return "post" + this.menuItems[index].id;
+    },
+    subMenuClass(index) {
+    	//console.log(index);
+    	return "post" + 1 + index;
+    }
+
+
+    }	
+});
